9494fc26a763c9a58a688be2d672d6f5c5fc4d9a
Gorgi Kosev <gorgi.kosev@gmail.com>
2014-01-17 19:49:21 +0100

    added moar tests

--- test/index.js
+++ test/index.js
@@ -43,4 +43,30 @@ t.test('function fails', function(t) {
 
 })
 
+t.test('single close fails', function(t) {
+    var r = [create({}), create({failClose:true}), create({})];
+    return using(r[0], r[1], r[2], function(r) {
+        t.ok(true, 'should execute fn');
+    }).then(function(e) {
+        return Promise.join(r[0], r[2]).spread(function(r0, r2) {
+            t.ok(r0.closed && r2.closed, "should not leak other resources");
+        });
+    });
+
+})
+
+
+t.test('all ok', function(t) {
+    var r1 = create({first:true}), r2 = create({second:true});
+    return using(r1, r2, function(r1, r2) {
+        t.ok(r2.opt, 'resource should have  opt member');
+        return [r2, r1];
+    }).spread(function(r2, r1) {
+        t.ok(r1.opt.first, 'should be first');
+        t.ok(r2.opt.second, 'should be second');
+        t.ok(r1.closed && r2.closed, 'both should be closed');
+    });
+
+})
+
 
